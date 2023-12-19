f12de02a369332a542646d007cef29626ca7a259
Arj09 <arjunkushwah100199@gmail.com>
2023-12-19 20:38:57 +0530

    start

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Arjun")
+const express = require("express");
+
+
+const app = express()
+const port = 5000
+
+app.listen(port, (req, res)=>{
+    console.log('Server is running on  port', port)
+
+})
