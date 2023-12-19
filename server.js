0a6a26972ab23a4c160da808491916c5de480ab8
Arj09 <arjunkushwah100199@gmail.com>
2023-12-19 20:50:59 +0530

    add

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res)=>{
     res.json({message:"your sucessful in your mission"})
 
 })
+app.get('/api/user', (req, res)=>{
+    res.json({message:"Please login in system "})
+
+})
 
 
 
