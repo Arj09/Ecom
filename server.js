e66f15d4ecc2b307b7499dd33754b91d54cdf2b7
Arj09 <arjunkushwah100199@gmail.com>
2023-12-19 20:44:32 +0530

    server add

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const express = require("express");
 const app = express()
 const port = 5000
 
+app.get('/', (req, res)=>{
+    res.json({message:"your sucessful in your mission"})
+
+})
+
+
+
 app.listen(port, (req, res)=>{
     console.log('Server is running on  port', port)
 
